48ccd0e1cef25eca6e6ef6f19090e2d82cf2a2ad
Manoj Kumar <manoj.kumar@manojvsr-32.successivetech.com>
2025-07-23 12:04:08 +0530

    assignment2 updated

--- src/assignment-2/app.ts
+++ src/assignment-2/app.ts
@@ -1,17 +1,10 @@
 // src/assignment-2/app.ts (use the controller)
 
 import express from "express";
-import cookieParser from "cookie-parser";
 import { getUsers } from "../controllers/userControllers";
 
-const app = express();
-const PORT = 3500;
-
-app.use(cookieParser());
+const router = express.Router();
 
 // Route: /mock
-app.get("/mock", getUsers);
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+router.get("/mock", getUsers);
+export default router
--- src/routes/assigRouter.ts
+++ src/routes/assigRouter.ts
@@ -1,13 +1,14 @@
 
 
 import express from 'express';
-import router from '../assignment-1/app';
+import assign1router from '../assignment-1/app';
+import assign2router from '../assignment-2/app'
 // import router from '../assignment-2/app';
 
 const assignRouter = express.Router();
 
 // Example GET route
 
-assignRouter.use("/assignment-1", router)
-assignRouter.use("/assignment-2",router)
+assignRouter.use("/assignment-1", assign1router)
+assignRouter.use("/assignment-2", assign2router)
 export default assignRouter;
