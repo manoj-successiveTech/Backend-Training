dbd92d962b9b9569e646dc69decde1415a818701
Manoj Kumar <manoj.kumar@manojvsr-32.successivetech.com>
2025-07-30 20:23:31 +0530

    assignment 3 updated

--- src/routes/assignRouter.ts
+++ src/routes/assignRouter.ts
@@ -5,7 +5,6 @@ import assign1router from '../assignment-1/app';
 import assign2router from '../assignment-2/app';
 import assign3router from '../assignment-3/app';
 
-// import router from '../assignment-2/app';
 
 const assignRouter = express.Router();
 
@@ -13,6 +12,6 @@ const assignRouter = express.Router();
 
 assignRouter.use("/assignment-1", assign1router)
 assignRouter.use("/assignment-2", assign2router)
-assignRouter.use("/assignment-3",assign3router)
+assignRouter.use("/assignment-3", assign3router)
 
 export default assignRouter;
